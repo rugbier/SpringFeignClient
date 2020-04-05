6e0d258ec8cbe31babc6feb80abfae3a9a901a36
rugbier <rugbier@hotmail.com>
2020-04-05 17:07:49 +0200

    Added Mapstruct to client Service

---
+++ ClientService/src/main/java/com/example/spring/cloud/feign/clientservice/mapper/ClientMapper.java
@@ -0,0 +1,17 @@
+package com.example.spring.cloud.feign.clientservice.mapper;
+
+import com.example.spring.cloud.feign.clientservice.dto.ClientDTO;
+import com.example.spring.cloud.feign.clientservice.model.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ClientMapper {
+
+    ClientMapper INSTANCE = Mappers.getMapper( ClientMapper.class );
+
+    @Mapping(target = "orders", ignore = true)
+    ClientDTO clientToDTO(Client client);
+}
+
--- ClientService/src/main/java/com/example/spring/cloud/feign/clientservice/service/ClientService.java
+++ ClientService/src/main/java/com/example/spring/cloud/feign/clientservice/service/ClientService.java
@@ -2,6 +2,7 @@ package com.example.spring.cloud.feign.clientservice.service;
 
 import com.example.spring.cloud.feign.clientservice.dto.ClientDTO;
 import com.example.spring.cloud.feign.clientservice.dto.OrderDTO;
+import com.example.spring.cloud.feign.clientservice.mapper.ClientMapper;
 import com.example.spring.cloud.feign.clientservice.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,13 +21,7 @@ public class ClientService {
     public List<ClientDTO> getAllClients(){
         List<ClientDTO> list = new ArrayList<>();
         clientRepository.findAll().forEach(c -> {
-            ClientDTO client = new ClientDTO();
-            client.setId(c.getId());
-            client.setEmail(c.getEmail());
-            client.setId_card(c.getId_card());
-            client.setName(c.getName());
-            client.setSurname(c.getSurname());
-            client.setPhone(c.getPhone());
+            ClientDTO client = ClientMapper.INSTANCE.clientToDTO(c);
             List<OrderDTO> orders = orderService.getOrdersByClient((int)c.getId());
             client.setOrders(orders);
             list.add(client);
