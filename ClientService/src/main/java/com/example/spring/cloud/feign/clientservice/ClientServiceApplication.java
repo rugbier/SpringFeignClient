b006e7d6e1f8d0d544557bdb5288e0374f53d283
rugbier <rugbier@hotmail.com>
2020-04-08 17:31:13 +0200

    Added Hystrix

--- ClientService/src/main/java/com/example/spring/cloud/feign/clientservice/ClientServiceApplication.java
+++ ClientService/src/main/java/com/example/spring/cloud/feign/clientservice/ClientServiceApplication.java
@@ -3,6 +3,9 @@ package com.example.spring.cloud.feign.clientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,6 +17,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan("com.example.spring.cloud.feign.clientservice")
 @EnableJpaRepositories("com.example.spring.cloud.feign.clientservice")
 @EnableTransactionManagement
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ClientServiceApplication {
 
     public static void main(String[] args) {
